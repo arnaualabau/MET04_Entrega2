15f00148344c8fecaf5f7a46142c64203971b3d9
Arnau <arnau.aas@gmail.com>
2021-12-20 20:22:06 +0100

    Initial commit

---
+++ app/src/main/java/com/example/met04_test/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.met04_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+}
