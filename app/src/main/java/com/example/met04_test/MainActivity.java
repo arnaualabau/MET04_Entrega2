d4054e9ec0a4e3d0e844c3a1d1528e40b78cd815
Sergi Ubach <sergi.ubachi@students.salle.url.edu>
2021-12-28 21:36:56 +0100

    Ja va
    - Es pot interactuar entre les dues activities
    Es pot millorar l'estetica i tal pero aquest commit ja es podria entregar crec

--- app/src/main/java/com/example/met04_test/MainActivity.java
+++ app/src/main/java/com/example/met04_test/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.met04_test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import java.io.*;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -8,18 +10,23 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.calculator);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        btn = findViewById(R.id.calculator);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setContentView(R.layout.activity2_main);
+                System.out.print("Button");
+                //setContentView(R.layout.activity2_main);
+                startActivity(new Intent(MainActivity.this, Activity2.class));
             }
         });
+
     }
 
     @Override
